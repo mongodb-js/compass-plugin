04388a858e9a9cd52141b913994626fac227a469
Durran Jordan <durran@gmail.com>
2017-05-02 20:09:55 -0400

    Update for new lifecycle methods in generated store

--- template/src/stores/index.js
+++ template/src/stores/index.js
@@ -1,14 +1,12 @@
 const Reflux = require('reflux');
 const {{pascalcase name}}Actions = require('../actions');
 const StateMixin = require('reflux-state-mixin');
-const { DataServiceStore } = require('mongodb-data-service');
 
 const debug = require('debug')('mongodb-compass:stores:{{slugcase name}}');
 
 /**
  * {{capitalcase name}} store.
  */
-
 const {{pascalcase name}}Store = Reflux.createStore({
   /**
    * adds a state to the store, similar to React.Component's state
@@ -25,10 +23,23 @@ const {{pascalcase name}}Store = Reflux.createStore({
    * Initialize everything that is not part of the store's state.
    */
   init() {
-    DataServiceStore.listen((error, dataService) => {
-      // This will happen when the data service is initialized and can be used
-      // if no error is passed.
-    });
+  },
+
+  /**
+   * This method is called when all packages are activated. Can be
+   * removed if not needed.
+   */
+  onActivated() {
+    // Maybe go to the global.hadronApp.appRegistry and listen to other stores or actions?
+  },
+
+  /**
+   * This method is called when the data service is finished connecting.
+   * If there is an error, the data service is not connected. If not, the
+   * passed data service is connected.
+   */
+  onConnected(error, dataService) {
+    // this.dataService = dataService;
   },
 
   /**
