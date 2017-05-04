3a59ff69fd3086bcd8ebbcd8dc30686d80b7db31
Durran Jordan <durran@gmail.com>
2017-05-04 14:00:48 -0400

    Adding app registry

--- template/electron/renderer/index.js
+++ template/electron/renderer/index.js
@@ -3,12 +3,14 @@ require('babel-register')({ extensions: ['.jsx'] });
 const app = require('hadron-app');
 const React = require('react');
 const ReactDOM = require('react-dom');
+const AppRegistry = require('hadron-app-registry');
 
 const { DataServiceStore, DataServiceActions } = require('mongodb-data-service');
 const Connection = require('mongodb-connection-model');
 
 const {{pascalcase name}}Component = require('../../lib/components');
 const {{pascalcase name}}Store = require('../../lib/stores');
+const {{pascalcase name}}Actions = require('../../lib/actions');
 
 const CONNECTION = new Connection({
   hostname: '127.0.0.1',
@@ -29,5 +31,6 @@ DataServiceStore.listen((error, ds) => {
 global.hadronApp = app;
 global.hadronApp.appRegistry = new AppRegistry();
 global.hadronApp.appRegistry.registerStore('{{pascalcase name}}.Store', {{pascalcase name}}Store);
+global.hadronApp.appRegistry.registerAction('{{pascalcase name}}.Actions', {{pascalcase name}}Actions);
 
 DataServiceActions.connect(CONNECTION);
