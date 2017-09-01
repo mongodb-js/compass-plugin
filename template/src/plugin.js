4aa8a8a360b664f39bbbd00a194af4dafa4e800a
Matt Fairbrass <matt.fairbrass@mongodb.com>
2017-09-01 11:49:45 +1000

    Significantly reduced the output size of the bundle file by selecting common Compass dependencies as peerDependencies. Updated webpack configurations to fix linting errors. Removed prod electron bundle output from webpack configuration as this was causing builds to indefinitely hang on larger plugin code bases.

--- template/src/plugin.js
+++ template/src/plugin.js
@@ -4,13 +4,6 @@ import {{pascalcase name}} from 'components/{{name}}';
 import store from 'stores';
 import actions from 'actions';
 
-// Import global less file. Note: these styles WILL NOT be used in compass, as compass provides its own set
-// of global styles. If you are wishing to style a given component, you should be writing a less file per
-// component as per the CSS Modules ICSS spec. @see components/ToggleButton for an example.
-if (process.env.NODE_ENV !== 'test') {
-  require('less/index.less');
-}
-
 class Plugin extends Component {
   static displayName = '{{pascalcase name}}Plugin';
 
