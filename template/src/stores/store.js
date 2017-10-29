85a68bda985dce920a8ee632c5bf95f3fe2c2cbd
Durran Jordan <durran@gmail.com>
2017-10-29 16:25:17 +0100

    Better app registry event comments

--- template/src/stores/store.js
+++ template/src/stores/store.js
@@ -18,12 +18,13 @@ const {{pascalcase name}}Store = Reflux.createStore({
   mixins: [StateMixin.store],
 
   /**
-   * listen to all actions defined in ../actions/index.jsx
+   * listen to all actions defined in ../actions/index.js
    */
   listenables: {{pascalcase name}}Actions,
 
   /**
-   * Initialize everything that is not part of the store's state.
+   * Initialize everything that is not part of the store's state. This happens
+   * when the store is required and instantiated. Stores are singletons.
    */
   init() {
   },
@@ -41,12 +42,49 @@ const {{pascalcase name}}Store = Reflux.createStore({
   // eslint-disable-next-line no-unused-vars
   onActivated(appRegistry) {
     // Events emitted from the app registry:
-    // appRegistry.on('application-intialized', (version) => return true);
-    // appRegistry.on('data-service-intialized', (dataService) => return true);
-    // appRegistry.on('data-service-connected', (error, dataService) => return true);
-    // appRegistry.on('collection-changed', (namespace) => return true);
-    // appRegistry.on('database-changed', (namespace) => return true);
-    // appRegistry.on('query-applied', (queryState) => return true);
+    //
+    // appRegistry.on('application-intialized', (version) => {
+    //   // Version is string in semver format, ex: "1.10.0"
+    // });
+    //
+    // appRegistry.on('data-service-intialized', (dataService) => {
+    //   // dataService is not yet connected. Can subscribe to events.
+    //   // DataService API: https://github.com/mongodb-js/data-service/blob/master/lib/data-service.js
+    // });
+    //
+    // appRegistry.on('data-service-connected', (error, dataService) => {
+    //   // dataService is connected or errored.
+    //   // DataService API: https://github.com/mongodb-js/data-service/blob/master/lib/data-service.js
+    // });
+    //
+    // appRegistry.on('collection-changed', (namespace) => {
+    //   // The collection has changed - provides the current namespace.
+    //   // Namespace format: 'database.collection';
+    //   // Collection selected: 'database.collection';
+    //   // Database selected: 'database';
+    //   // Instance selected: '';
+    // });
+    //
+    // appRegistry.on('database-changed', (namespace) => {
+    //   // The database has changed.
+    //   // Namespace format: 'database.collection';
+    //   // Collection selected: 'database.collection';
+    //   // Database selected: 'database';
+    //   // Instance selected: '';
+    // });
+    //
+    // appRegistry.on('query-applied', (queryState) => {
+    //   // The query has changed and the user has clicked "filter" or "reset".
+    //   // queryState format example:
+    //   //   {
+    //   //     filter: { name: 'testing' },
+    //   //     project: { name: 1 },
+    //   //     sort: { name: -1 },
+    //   //     skip: 0,
+    //   //     limit: 20,
+    //   //     ns: 'database.collection'
+    //   //   }
+    // });
   },
 
   /**
